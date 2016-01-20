967b5d07cb41d15ece514502e9820b252c9d43c7
Syed (Sadat) Nazrul <ssnazrul@eng.ucsd.edu>
2016-01-20 14:11:31 -0800

    Update SalesCube.sql

--- DSE201/SalesCube.sql
+++ DSE201/SalesCube.sql
@@ -1,3 +1,11 @@
+/*
+
+Consider a database that captures customers with a name and state; states with a name; products that
+have a list price, a name and belong to a category; categories have names and descriptions; sales of a
+product by a customer capturing quantity and price paid (may be discounted).
+
+*/
+
 --Entities
 
 CREATE TABLE customers (
